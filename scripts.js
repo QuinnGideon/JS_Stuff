12cf8e568df38d9fdbd3c73024bd970aee60b613
Quinn Gideon <Quinn.Gideon@gmail.com>
2016-08-01 07:47:28 -0400

    added document color changing script

--- scripts.js
+++ scripts.js
@@ -2,6 +2,9 @@
 var currentH1 = document.getElementById('heading1');
 // This variable attaches to the input box
 var inputValue = document.getElementById('boxInput');
+var myDoc = document.body.style;
+// This variable sets up the REGEX
+var myRE = /blue/;
 
 
 // This function allows you to change the h1 after entering your argument
@@ -12,4 +15,13 @@ function changeH1(param) {
 // This function calls the changeH1 function, and uses the inputValue as arguments
 function changeContent() {
   changeH1(inputValue.value);
+}
+
+// This function listens for the input and checks against the regex variable to change color
+function changeColor() {
+  if (myRE.test(inputValue.value)) {
+    myDoc.backgroundColor = "blue";
+  } else {
+    myDoc.backgroundColor = "white";
+  }
 }
